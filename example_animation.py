83d8e276f2764be069efea22b9794bf24840ec40
LIYIZHOU <495903541@qq.com>
2022-03-14 00:16:38 +0800

    update example_animation.py
    试试example_animation.py
    是一个0.3c从左到右的球体

--- example_animation.py
+++ example_animation.py
@@ -4,7 +4,7 @@ from my_raytracer import *
 shape1= Sphere(.5, get_checkerboard_color_func(WHITE, GRAY))
 #shape1= Cube(1,1,1, get_cubical_checkerboard_color_func(RED_E, WHITE, .1))
 shape2= Plane(vec3(0,0,0), vec3(0,1,0), diffuse_color_function= get_cubical_checkerboard_color_func(GREY, WHITE, .15))
-beta1 = (0, 0, 0)
+beta1 = (0.3, 0, 0)
 beta2 = (0, 0, 0)
 offset1 = vec4(0, 0, 0, 1.5)
 offset2 = vec4(0, 0, -.5, 0)
@@ -14,12 +14,14 @@ object2= MovingObject(shape2, beta2, offset2)
 movingobjects= [object1, object2]
 
 scene= Scene(movingobjects)
-frames= 10
-t00= time.time()
-for u, frame_count in np.linspace([0,1],[0.5,frames], frames):
-    print('开始渲染第%s帧...' % int(frame_count), end= '')
-    object1.set_beta((u, 0, 0))
-    scene.generate_image(1, os.path.join('./render', f"{int(frame_count)}.png"))
-    t1= time.time()
-    print("预计剩余%i分钟" % ((t1-t00)/frame_count*(frames-frame_count)/60))
+scene.generate_animation(-6, 5, 10, 'render1')
+
+#frames= 10
+#t00= time.time()
+#for u, frame_count in np.linspace([0,1],[0.5,frames], frames):
+#    print('开始渲染第%s帧...' % int(frame_count), end= '')
+#    object1.set_beta((0, 0, u))
+#    scene.generate_image(1, os.path.join('render', f"{int(frame_count)}.png"))
+#    t1= time.time()
+#    print("预计剩余%i分钟" % ((t1-t00)/frame_count*(frames-frame_count)/60))
 print('渲染完成')
